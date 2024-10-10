ea42165309d8c9e679f111d3dc60174c38e2b514
Rallystyle <reelikav.2006@gmail.com>
2024-10-10 11:18:34 +0300

    tunnitöö tehtud

--- praktiline_too_10.10.py
+++ praktiline_too_10.10.py
@@ -11,8 +11,8 @@ a = float(input("Sisesta oma a:"))
 b = float(input("Sisesta oma b:"))
 # meie ülesandeks on leida hüpoteenus c, kolmnurga ümbermõõt ja pindala NB(vastused peavad olema ümardatud sajandikeni)
 
-hüpotenuusc = ()
+c = math.sqrt(a**2 + b**2)
+p = (a +b + c)
+S = (a * b)/2
 
-ümbermõõt = ()
-pindala = ()
-print(f"Lahendid on: hüpotunuus={hüpotenuus} ja ümbermõõt={ümbermõõt} ja pindala={pindala}")
+print(f"Lahendid on: hüpotunuus={round(c,2)} ja ümbermõõt={round(p,2)} ja pindala={round(S,2)}")
